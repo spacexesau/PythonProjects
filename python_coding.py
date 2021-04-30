b4720578b243d579b6b13d8b4988a5ff4db0721b
spacexesau <81427878+spacexesau@users.noreply.github.com>
2021-04-29 22:08:34 -0400

    Create python_coding.py
    For now, I'm creating and developing a project. This is the starting phases and I need to continue coming back with an idea.

---
+++ python_coding.py
@@ -0,0 +1,6 @@
+"""
+Here I'm starting to create a python project.
+Introduction: I'm not sure what I want to create but 
+this will be start of creating something marvelous.
+*Note: need some brainstorming ideas!
+"""
